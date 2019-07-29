8e424cfebd73d35bbe5bdf545e806443510f0593
Yanaway, Amy <ayanaway@wbhq.com>
2019-07-29 13:56:25 -0500

    Create CelestialObjectController get actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -19,5 +19,50 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var result = _context.CelestialObjects.Find(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+
+            result.Satellites = _context.CelestialObjects.Where(oo => oo.OrbitedObjectId == id).ToList();
+
+            return Ok(result);
+
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObj = _context.CelestialObjects.Find(name);
+
+            if (celestialObj == null)
+            {
+                return NotFound();
+            }
+
+            celestialObj.Satellites = _context.CelestialObjects.Where(oo => oo.OrbitedObjectId == celestialObj.Id).ToList();
+
+            return Ok(celestialObj);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjs = _context.CelestialObjects.ToList();
+
+            foreach(var celestialObj in celestialObjs)
+            {
+                celestialObj.Satellites = celestialObjs.Where(oo => oo.OrbitedObjectId == celestialObj.Id).ToList();
+            }
+
+            return Ok(celestialObjs);
+        }
+
     }
 }
